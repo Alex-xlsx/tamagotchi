0ee69db2bfed3802e8880a782f4acf96af56a504
denise williams <denisewilliams@denises-MacBook-Pro.local>
2021-04-19 19:48:00 -0400

    commit prework outline for what the project should do

---
+++ app.js
@@ -0,0 +1,50 @@
+console.log('test');
+
+//REQUIREMENTS
+//class for tamagotchi
+//changing metrics (at least 4)
+//interactablility with pet (at least 3)
+//name your pet
+//style the page
+//fail/success state
+//an avatar for the pet, 
+////this avatar should have some movement
+
+//Start with a creation process
+////use colors
+////{{{{RBG + RBG + RBG 
+/////RRR = Red / BBB = Blue / GGG = Green 
+/////RBB = Purple / RBG = white / RGG = yellow
+/////BRR = purple / BGG = cyan 
+/////GRR = yellow / GBB = cyan}}}}}
+//////////Red/Blue/Green/Purple/Cyan/Yellow/White
+////these colors are recorded and used for the final creature
+//name the creation
+//interact with the creation to set stats
+////basic selectors that grant stats. Similar to the operation of a regular tamagotchi. 
+///////petting = HP
+///////reading = Mana
+///////playing = Atk
+///////napping = Defense
+///////cleaning = Money
+///////each day you can perform 1 action, then you automatically feed the creature, then 1 more action, then it automatically sleeps. 
+///////you have 7 days of actions to take 
+///////////repeating 1 action too many times will make the creature bored (AKA the stats have caps)
+//{{{{{use the creation to battle
+/////generate enemies (a la Space Battle)
+/////use FF style combat
+/////can purchase things like potions?
+////////money is auto-used to buy potions until
+////////you're out}}}}}
+/////3 fights and then a boss? 
+//win/loss
+
+//{} notates that this feature is not requires
+/////another feature: make purchases BEFORE adventuring 
+//reference the square clicking game
+//reference the space battle game
+
+
+
+//the avatar could be made of a grind of roughly 20 small squares inside 1 larger div. That div could be moved around to move the avatar. Simple squash and stretch can be done on it
+
