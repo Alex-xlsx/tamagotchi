dcab82f91450ce5c935592fab8ed0f18b10eaeaf
denise williams <denisewilliams@denises-MacBook-Pro.local>
2021-04-22 13:11:41 -0400

    Currently working version with end, only missing the ability to loop. Going to try to fix that without destroying everything

--- app.js
+++ app.js
@@ -58,24 +58,55 @@ let blue = 0;
 
 function setTime(time){
     if (time === 0){
-        $('main').css('backgroundImage','none');
+        $('article').css('backgroundImage','none');
     } else if (time === 1) {
-        $('main').css('backgroundImage','url(./artAssets/8bitMorning.png)');
+        $('article').css('backgroundImage','url(./artAssets/8bitMorning.png)');
         morning();
     } else if (time === 2) {
-        $('main').css('backgroundImage','url(./artAssets/8bitDay.png)');
+        $('article').css('backgroundImage','url(./artAssets/8bitDay.png)');
         afternoon();
     } else if (time === 3) {
-        $('main').css('backgroundImage','url(./artAssets/8bitEvening.png)');
+        $('article').css('backgroundImage','url(./artAssets/8bitEvening.png)');
         evening();
     } else if (time === 4) {
-        $('main').css('backgroundImage','url(./artAssets/8bitNight.png)');
+        $('article').css('backgroundImage','url(./artAssets/8bitNight.png)');
         night()
     } else if (time === 5) {
-        $('main').css('backgroundImage','url(./artAssets/8bitSpace.png)');
+        $('article').css('backgroundImage','url(./artAssets/8bitSpace.png)');
     } else if (time === 6) {
-        $('main').css('backgroundImage','url(./artAssets/8bitCalm.png)');
+        $('article').css('backgroundImage','url(./artAssets/8bitCalm.png)');
     } 
+}
+function passTime(){
+    if (currentTime !== 4){
+        currentTime++;
+        setTime(currentTime);
+        bestFriend.sleepiness+= 1;
+        bestFriend.hunger+=1;
+        bestFriend.boredom+=1;
+    } else if (currentTime ===4){
+        currentTime = 1;
+        setTime(currentTime);
+        if (bestFriend.age === 10){
+            endGame();
+        }
+        bestFriend.age++;
+        bestFriend.sleepiness-= 3;
+        bestFriend.hunger+=1;
+        bestFriend.boredom+=1;
+        if (bestFriend.boredom > 5){
+            bestFriend.affection -= 4;
+        }
+        if (bestFriend.sleepiness > 5){
+            bestFriend.affection -= 4;
+        }
+        if (bestFriend.dirtiness > 5){
+            bestFriend.affection -= 4;
+        }
+        if (bestFriend.hunger > 5){
+            bestFriend.affection -= 4;
+        }   
+    }
 }
 
 const bestFriend = {
@@ -85,7 +116,9 @@ const bestFriend = {
     sleepiness : 0,
     dirtiness : 0,
     hunger : 0,
-    age : 0
+    age : 0,
+    width : 50,
+    height : 40
 }
 function testCheck (){
     console.log(bestFriend);
@@ -93,16 +126,28 @@ function testCheck (){
 function stats() {
     $('#name').text(`${bestFriend.name}`);
     $('#affection').text(`Affection: ${bestFriend.affection}`);
+    if (bestFriend.boredom < 0){
+        bestFriend.boredom = 0;
+    }
     $('#boredom').text(`Boredom: ${bestFriend.boredom}`);
+    if (bestFriend.sleepiness < 0){
+        bestFriend.sleepiness = 0;
+    }
     $('#sleepiness').text(`Sleepiness: ${bestFriend.sleepiness}`);
+    if (bestFriend.dirtiness < 0){
+        bestFriend.dirtiness = 0;
+    }
     $('#dirtiness').text(`Dirtiness: ${bestFriend.dirtiness}`);
+    if (bestFriend.hunger < 0){
+        bestFriend.hunger = 0;
+    }
     $('#hunger').text(`Hunger: ${bestFriend.hunger}`);
-    $('#time').text(`Age: ${bestFriend.age}`);
+    $('#time').text(`Day: ${bestFriend.age}`);
 }
 
 function makeFriend(assign,assign2){
     friendCount++
-    $('main').append($(`<div class="display animate-keyframe" id="character${friendCount}"></div>`));  
+    $('article').append($(`<div class="display animate-keyframe" id="character${friendCount}"></div>`));  
     for(let i = 0;i<60;i++){
         const pixel = $('<div class="sprite"></div>');
         $(`#character${friendCount}`).append(pixel);
@@ -116,9 +161,13 @@ function makeFriend(assign,assign2){
         } else if (i === 31 || i === 38){
             $(pixel).css('backgroundColor','pink');
         }
+        ///////FIND OUT WHY THIS MAKES INIFINITY BOUNCE
+        // $(`#character${friendCount}`).on('click',function(){
+        //     bouncy(1,bestFriend.width,bestFriend.height);
+        // });
     }
 }
-$('#start').on('mousedown',function(){
+$('#start').on('mousedown',function beginGame(){
     $('#start').css('backgroundColor','white');
     $('#start').css('color','gold');
 });
@@ -135,6 +184,7 @@ $('#start').on('click',function(){
             opacity: 0
         },2000,'linear',function(){
             $('#start').remove();
+            $('#optionHolder').empty();
             $('#text').text(`Hello there! You look like you could use a friend. Would you mind helping me make them?`)
             $('#optionHolder').append($('<button class="options" id="b1">Certainly!</button>'))
                 $('#b1').on('click',function(){
@@ -235,23 +285,79 @@ function creation(){
                 $('#text').text(`${bestFriend.name}? What a fantastic name! I think you two will be fast friends. Please take good care of ${bestFriend.name} from now on, okay?`);                
                 $('#optionHolder').append($(`<button class="options" id="b1">I can't wait</button>`)) 
                 $('#b1').on('click',function(){
+                    currentTime=1;
+                    setTime(currentTime);
                     exitCreation()
                 });
             }
         });
     });
 }
-
-function bouncy(bounces){
+//////////----------------BOUNCE FUNCTION. REPLACED WITH CSS???
+waitTime = 0;
+function delayHolder (delay) {
+    let timeWindow = window.setInterval(function (){
+        waitTime = 1;
+        waitTime --;
+        if (waitTime <= 0){
+            window.clearInterval(timeWindow);
+            $('#character1').addClass(`animate-keyframe`);
+            if(bestFriend.age >=3 && bestFriend.affection >=15){
+                $('#character1').css('animation-name','breathe2');
+                bestFriend.width = 75;
+                bestFriend.height = 60;
+            } 
+            if (bestFriend.age >=6 && bestFriend.affection >=30){
+                $('#character1').css('animation-name','breathe3');
+                bestFriend.width = 100;
+                bestFriend.height = 80;
+            }
+        }
+    }, delay);
+}
+function bouncy(bounces,x,y){
+    $('#character1').removeClass(`animate-keyframe`);
+    delayHolder(bounces*550);
     for (let i = 0;i<bounces;i++){
         $('#character1').animate({
-            marginBottom: 100
-        },200,'swing');
+            marginBottom: 10,
+            width: x*1.1,
+            height: y*.8
+        },100,'linear');
         $('#character1').animate({
-            marginBottom: 10
-        },150,'swing');
+            marginBottom: 100,
+            width: x*.8,
+            height: y*1.2
+        },100,'swing');
+        $('#character1').animate({
+            marginBottom: 120,
+            width: x*1.2,
+            height: y*1
+        },100,'linear');
+        $('#character1').animate({
+            marginBottom: 100,
+            width: x*.8,
+            height: y*1.2
+        },50,'swing');
+        $('#character1').animate({
+            marginBottom: 10,
+            width: x*.6,
+            height: y*1.4
+        },50,'swing');
+        $('#character1').animate({
+            marginBottom: 10,
+            width: x*1.4,
+            height: y*.3
+        },100,'linear');
+        $('#character1').animate({
+            marginBottom: 10,
+            width: x,
+            height: y
+        },50,'swing');
     }
-}
+} 
+
+//////////----------------BOUNCE FUNCTION. REPLACED WITH CSS???
 
 //reoccuring text prompts-------------START
 function morning(){
@@ -266,64 +372,177 @@ function evening(){
 function night(){
     $('#text').text(`My how time flies, night already! Anything you want to do before bed?`); 
 }
-
-    // $('#text').text(`Gently pet your soft friend. Increases Affection by 3, but increases sleepiness by 1`); 
-
-    // $('#text').text(`Read a story to ${bestFriend.name}. Increases Affection by 3, but increases Boredom by 1`); 
-
-    // $('#text').text(`Play a game with ${bestFriend.name}. Decreases boredom by 5, but also increases Dirtiness by 2 and increases sleepiness by 1`);
-
-    // $('#text').text(`Take a nap with your little friend. Decreases Sleepiness by 4, but increases boredom by 1`);
-
-    // $('#text').text(`Eat a healthy snack with ${bestFriend.name}. Decreases Hunger by 4, but also increases Dirtiness by by 1`);
-
-    // $('#text').text(`Give your friend a bath. Decreases Dirtiness by 5.`);
-
 //reoccuring text prompts-------------END
-
 function exitCreation(){
-    setTime(1);
-    bouncy(2);
+    stats();
+    bouncy(2,bestFriend.width,bestFriend.height);
     $('#optionHolder').empty();
-    $('#optionHolder').append($(`<button class="options" id="b1">Pet</button>`))
-    $('#optionHolder').append($(`<button class="options" id="b2">Read</button>`))
-    $('#optionHolder').append($(`<button class="options" id="b3">Play</button>`))
-    $('#optionHolder').append($(`<button class="options" id="b4">Feed</button>`))
-    $('#optionHolder').append($(`<button class="options" id="b5">Nap</button>`))
-    $('#optionHolder').append($(`<button class="options" id="b6">Clean</button>`))
-    $('#b1').on('click',function(){
+    $('#optionHolder').append($(`<button class="options" id="b1">Pet</button>`));
+    $('#optionHolder').append($(`<button class="options" id="b2">Read</button>`));
+    $('#optionHolder').append($(`<button class="options" id="b3">Play</button>`));
+    $('#optionHolder').append($(`<button class="options" id="b4">Feed</button>`));
+    $('#optionHolder').append($(`<button class="options" id="b5">Nap</button>`));
+    $('#optionHolder').append($(`<button class="options" id="b6">Clean</button>`));
+    //primary button functionality--------------------------------START
+    $('#b1').on('click',function(){//--------------BUTTON 1
         bestFriend.affection += 3;
         bestFriend.sleepiness +=1;
+        bouncy(1,bestFriend.width,bestFriend.height);
+        passTime();
         stats();
+        /////////////////////////THIS MOUSE ENTER/EXIT REPEATS 6 TIMES. HOW DO I MAKE THIS A FUNCTION WITHOUT IT AUTOCALLING ITSELF?
+    }).mouseenter(function(){
+        $('#text').text(`Gently pet your soft friend. Increases Affection by 3, but increases sleepiness by 1`);
+    }).mouseleave(function(){
+        if(currentTime === 1){
+            $('#text').text(`It's a beautiful morning! How would you like to play with your friend today?`);    
+        } else if (currentTime ===2){
+            $('#text').text(`It's a lovely afternoon. What would you like to do with your friend?`); 
+        } else if (currentTime ===3){
+            $('#text').text(`It's a splended evening. Would you like to do something with your friend?`); 
+        } else if (currentTime ===4){
+            $('#text').text(`My how time flies, night already! Anything you want to do before bed?`); 
+        }
     });
-    $('#b2').on('click',function(){
+    $('#b2').on('click',function(){//--------------BUTTON 2
         bestFriend.affection +=3;
         bestFriend.boredom +=1;
-        stats()
+        bouncy(1,bestFriend.width,bestFriend.height);
+        passTime();
+        stats();
+    }).mouseenter(function(){
+        $('#text').text(`Play a game with ${bestFriend.name}. Decreases boredom by 5, but also increases Dirtiness by 2 and increases sleepiness by 1`);
+    }).mouseleave(function(){
+        if(currentTime === 1){
+            $('#text').text(`It's a beautiful morning! How would you like to play with your friend today?`);    
+        } else if (currentTime ===2){
+            $('#text').text(`It's a lovely afternoon. What would you like to do with your friend?`); 
+        } else if (currentTime ===3){
+            $('#text').text(`It's a splended evening. Would you like to do something with your friend?`); 
+        } else if (currentTime ===4){
+            $('#text').text(`My how time flies, night already! Anything you want to do before bed?`); 
+        }
     });
-    $('#b3').on('click',function(){
+    $('#b3').on('click',function(){//--------------BUTTON 3
         bestFriend.boredom -= 5;
         bestFriend.dirtiness += 2;
         bestFriend.sleepiness += 1;
-        stats()
+        bouncy(1,bestFriend.width,bestFriend.height);
+        passTime();
+        stats();
+    }).mouseenter(function(){
+        $('#text').text(`Read a story to ${bestFriend.name}. Increases Affection by 3, but increases Boredom by 1`);
+    }).mouseleave(function(){
+        if(currentTime === 1){
+            $('#text').text(`It's a beautiful morning! How would you like to play with your friend today?`);    
+        } else if (currentTime ===2){
+            $('#text').text(`It's a lovely afternoon. What would you like to do with your friend?`); 
+        } else if (currentTime ===3){
+            $('#text').text(`It's a splended evening. Would you like to do something with your friend?`); 
+        } else if (currentTime ===4){
+            $('#text').text(`My how time flies, night already! Anything you want to do before bed?`); 
+        }
     });
-    $('#b4').on('click',function(){
-        bestFriend.sleepiness -= 4;
-        bestFriend.boredom += 1;
-        stats()
-    });
-    $('#b5').on('click',function(){
+    $('#b4').on('click',function(){//--------------BUTTON 4
         bestFriend.hunger -= 4;
         bestFriend.dirtiness += 1;
-        stats()
+        bouncy(1,bestFriend.width,bestFriend.height);
+        passTime();
+        stats();
+    }).mouseenter(function(){
+        $('#text').text(`Eat a healthy snack with ${bestFriend.name}. Decreases Hunger by 4, but also increases Dirtiness by by 1`);
+    }).mouseleave(function(){
+        if(currentTime === 1){
+            $('#text').text(`It's a beautiful morning! How would you like to play with your friend today?`);    
+        } else if (currentTime ===2){
+            $('#text').text(`It's a lovely afternoon. What would you like to do with your friend?`); 
+        } else if (currentTime ===3){
+            $('#text').text(`It's a splended evening. Would you like to do something with your friend?`); 
+        } else if (currentTime ===4){
+            $('#text').text(`My how time flies, night already! Anything you want to do before bed?`); 
+        }
     });
-    $('#b6').on('click',function(){
+    $('#b5').on('click',function(){ //--------------BUTTON 5
+        bestFriend.sleepiness -= 4;
+        bestFriend.boredom += 1;
+        bouncy(1,bestFriend.width,bestFriend.height);
+        passTime();
+        stats();
+    }).mouseenter(function(){
+        $('#text').text(`Take a nap with your little friend. Decreases Sleepiness by 4, but increases boredom by 1`);
+    }).mouseleave(function(){
+        if(currentTime === 1){
+            $('#text').text(`It's a beautiful morning! How would you like to play with your friend today?`);    
+        } else if (currentTime ===2){
+            $('#text').text(`It's a lovely afternoon. What would you like to do with your friend?`); 
+        } else if (currentTime ===3){
+            $('#text').text(`It's a splended evening. Would you like to do something with your friend?`); 
+        } else if (currentTime ===4){
+            $('#text').text(`My how time flies, night already! Anything you want to do before bed?`); 
+        }
+    });
+    $('#b6').on('click',function(){//--------------BUTTON 6
         bestFriend.dirtiness -= 5;
-        stats()
+        bouncy(1,bestFriend.width,bestFriend.height);
+        passTime();
+        stats();
+    }).mouseenter(function(){
+        $('#text').text(`Give your friend a bath. Decreases Dirtiness by 5.`);
+    }).mouseleave(function(){
+        if(currentTime === 1){
+            $('#text').text(`It's a beautiful morning! How would you like to play with your friend today?`);    
+        } else if (currentTime ===2){
+            $('#text').text(`It's a lovely afternoon. What would you like to do with your friend?`); 
+        } else if (currentTime ===3){
+            $('#text').text(`It's a splended evening. Would you like to do something with your friend?`); 
+        } else if (currentTime ===4){
+            $('#text').text(`My how time flies, night already! Anything you want to do before bed?`); 
+        }
     });
+    //primary button functionality--------------------------------END
+}
+
+function endGame(){
+    $('#optionHolder').empty();
+    $('#text').empty();
+    currentTime = 6;
+    setTime(currentTime);
+    $('#text').text('Looks like our time is up! Lets see how your buddy has grown.')
+    $('#optionHolder').append($(`<button class="options" id="b1">So soon?</button>`));
+        $('#b1').on('click',function(){
+            $('#optionHolder').empty();
+            if (bestFriend.affection > 40){
+                $('#text').text('It looks like you really did a great job making your buddy happy. Great job!')
+                $('#optionHolder').append($(`<button class="options" id="b1">Thank you. We had a lot of fun!</button>`));
+                    $('#b1').on('click',function(){
+                        $('#optionHolder').empty();
+                        $('#text').text('HAPPY ENDING')
+                        $('#optionHolder').append($(`<button class="options" id="b1">Thanks for playing! Start new game?</button>`));
+                            $('#b1').on('click',function(){
+                            resetGame();
+                            });
+                    });                
+            } else {
+                $('#text').text(`It seems like you and your buddy could've been better friends. Perhaps you should try again?`)
+                $('#optionHolder').append($(`<button class="options" id="b1">I'll try harder next time!</button>`));
+                    $('#b1').on('click',function(){
+                        resetGame();
+                    });        
+            }
+
+        });
 }
 
 
-
-//make some console logs to see if we can figure out how to target / remove ".animate-keyframe"
-//I think we need to remove/add it any time we want to change height/width
+function resetGame (){
+    $('#text').empty();
+    $('#optionHolder').empty();
+    $('#name').text(`-`);
+    $('#affection').text(`-`);
+    $('#boredom').text(`-`);
+    $('#sleepiness').text(`-`);
+    $('#dirtiness').text(`-`);
+    $('#hunger').text(`-`);
+    $('#time').text(`-`);
+    beginGame();
+}
